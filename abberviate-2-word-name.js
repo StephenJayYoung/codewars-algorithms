3d04ab037b82cb8c996b7c4fac001c189f0e5d3e
Stephen Young <stephenjayyoung@gmail.com>
2020-05-24 08:53:03 -0700

    update to change all initials to uppercase

--- abberviate-2-word-name.js
+++ abberviate-2-word-name.js
@@ -1,9 +1,9 @@
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 function abbrevName(name){
-  let firstInit = name.charAt(0);
+  let firstInit = name.charAt(0).toUpperCase();
   let space = name.indexOf(' ');
   let secondInitPosition = space + 1;
-  let secondInit = name.charAt(secondInitPosition);
+  let secondInit = name.charAt(secondInitPosition).toUpperCase();
   let firstAndLast = `${firstInit}.${secondInit}` 
   return firstAndLast;
 }
