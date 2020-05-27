0a862f53f3777d6496216cbb135fa7af0132a8c8
Stephen Young <stephenjayyoung@gmail.com>
2020-05-27 12:12:50 -0700

    reversed sequence kata

--- reversed-sequence.js
+++ reversed-sequence.js
@@ -1,1 +1,10 @@
-reversed-sequence.js
+// Get the number n (n>0) to return the reversed sequence from n to 1.
+// Example : n=5 >> [5,4,3,2,1]
+const reverseSeq = n => {
+  let arr = [];
+  for (i = n; i > 0; i--){
+    arr.push(i);
+  }
+  return arr;
+};
+reverseSeq(5);
