f01b698e06735edce9a256a8f810e9a20759545b
Stephen Young <stephenjayyoung@gmail.com>
2020-06-02 15:09:43 -0700

    what is between kata in es6

---
+++ remove-string-spaces.js
@@ -0,0 +1,5 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+const noSpace = x =>{
+  return x.split(" ").join("");
+}
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B');
---
+++ what-is-between.js
@@ -0,0 +1,8 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+const between = (a,b) =>{
+  let arr = []
+  for (i = a; i <= b; i++){
+    arr.push(i);
+  }
+  return arr;
+}
